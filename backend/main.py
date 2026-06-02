3be3a7fd786dbcb0eb2fee582ff7e0e8be7c4030
abhijay-10 <parasharabhijay@gmail.com>
2026-06-02 15:57:56 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1215,4 +1215,5 @@ def get_tracker_insights(
     }
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
